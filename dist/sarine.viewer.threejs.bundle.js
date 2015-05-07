981045253d43f731f623e7d0de04596f4f166ffd
SarineColor <github@sarine.com>
2015-05-07 15:26:52 +0300

    deploy 0.1.0

--- dist/sarine.viewer.threejs.bundle.js
+++ dist/sarine.viewer.threejs.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.svg - v0.1.1 -  Monday, May 4th, 2015, 4:53:22 PM 
+sarine.viewer.threejs - v0.1.0 -  Thursday, May 7th, 2015, 3:26:41 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
