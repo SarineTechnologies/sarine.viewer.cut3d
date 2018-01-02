a65f25de2c599fb936f6235f2186116b7da8e7e6
ronishoham <roni.shoham@sarine.com>
2018-01-02 16:38:13 +0200

    task 104352

--- GruntfileBundle.js
+++ GruntfileBundle.js
@@ -3,6 +3,8 @@ module.exports = function(grunt) {
 
     require('load-grunt-tasks')(grunt)
     var target = grunt.option('target') || "";
+	var config = {};
+	config.dist = decideDist();
 
     grunt.initConfig({
         config: grunt.file.readJSON("package.json"),
@@ -36,6 +38,14 @@ module.exports = function(grunt) {
             bundle: {
                 dest: target + 'dist/<%= config.name %>.config',
                 src: [target + '<%= config.name %>.config']
+            },
+            dist_root_files: {
+                files: [{
+                        cwd: 'dist/',
+                        src: '**',
+                        dest: config.dist.root,
+                        expand: true
+                    }]
             }
         }
     });
@@ -45,7 +55,8 @@ module.exports = function(grunt) {
         'coffee',// Compile CoffeeScript files to JavaScript + concat + map
         'uglify',//min + banner + remove comments + map    
         'copyVersion',
-        'copy:bundle'
+        'copy:bundle',
+        'copy:dist_root_files'
     ]);
     grunt.registerTask('copyVersion' , 'copy version from package.json to sarine.viewer.clarity.config' , function (){
         var packageFile = grunt.file.readJSON(target + 'package.json');
@@ -60,4 +71,24 @@ module.exports = function(grunt) {
         copyFile.version = packageFile.version;
         grunt.file.write(configFileName , JSON.stringify(copyFile));
     });
+
+    function decideDist()
+    {
+        if(process.env.buildFor == 'deploy')
+        {
+            grunt.log.writeln("dist is github folder");
+
+            return {
+                root: 'app/dist/'
+            }
+        }
+        else
+        {
+            grunt.log.writeln("dist is local");
+
+            return {
+                root: '../../../dist/content/viewers/atomic/v1/js/'
+            }
+        }
+    }
 };
